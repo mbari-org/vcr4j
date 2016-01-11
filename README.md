c549363a1a1d6a6f393c2ec55a6b9c58863cd3aa
Brian Schlining <bschlining@gmail.com>
2016-01-11 14:34:29 -0800

    VARS-749: Prepping vcr4j-2.0.2 for release

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-vcr4j
+# vcr4j
 =====
 
 Java API for communicating with video tape recorders that support the Sony 9-pin protocol
 
 ## Overview
 
-The vcr4j project is a Java API for communicating with video cassette recorders (VCR) that support the Sony 9-pin communication protocol over RS422 (a serial port). It was originally developed by the [Monterey Bay Aquarium Research Institute](http://www.mbari.org) in support of the [Video Annotation and Reference System](https://github.com/hohonuuli/vars) (VARS). 
+The vcr4j project is a Java API for communicating with video cassette recorders (VCR) that support the Sony 9-pin communication protocol over RS422 (a serial port). It was originally developed by the [Monterey Bay Aquarium Research Institute](http://www.mbari.org) in support of the [Video Annotation and Reference System](https://github.com/hohonuuli/vars) (VARS).
 
+## To Build:
+
+	  mvn clean install
+
+The resulting jar can be found in the target directory
---
+++ src/main/java/org/mbari/vcr4j/rs422/RXTX.java
@@ -0,0 +1,53 @@
+package org.mbari.vcr4j.rs422;
+
+import java.io.File;
+import org.mbari.nativelib.Native;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Loads the native RXTX libraries needed. Just call:
+ *
+ * <pre>
+ * RXTX.setup();
+ * </pre>
+ */
+public class RXTX {
+  public static final String LIBRARY_NAME = "rxtxSerial";
+  private static final Logger log = LoggerFactory.getLogger(RXTX.class);
+
+  public static void setup() {
+    try {
+      System.loadLibrary(LIBRARY_NAME);
+    }
+    catch (UnsatisfiedLinkError e) {
+      extractAndLoadNativeLibraries();
+    }
+  }
+
+  private static void extractAndLoadNativeLibraries() {
+    String libraryName = System.mapLibraryName(LIBRARY_NAME);
+    String os = System.getProperty("os.name");
+
+    if (libraryName != null) {
+      File libraryHome = getLibraryHome();
+      if (!libraryHome.exists()) {
+        libraryHome.mkdirs();
+      }
+      if (!libraryHome.canWrite()) {
+        throw new RuntimeException("Unable to extract native libary to " + libraryHome +
+          ". Verify that you have write access to that directory");
+      }
+      new Native(LIBRARY_NAME, "native", libraryHome, RXTX.class.getClassLoader());
+    }
+    else {
+      log.error("A native RXTX library for your platform is not available. " +
+        "You will not be able to control your VCR");
+    }
+  }
+
+  private static File getLibraryHome() {
+    File tempDir = new File(System.getProperty("java.io.tmpdir"));
+    return new File(tempDir, "vcr4j");
+  }
+}
