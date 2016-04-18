80df8476c392859391fd72f109b0e0fe21518b38
Brian Schlining <bschlining@gmail.com>
2016-04-18 16:33:47 -0700

    Adding controls for VLC.

---
+++ vcr4j-vlcj/src/main/java/org/mbari/vcr4j/vlcj/MediaPlayerState.java
@@ -0,0 +1,12 @@
+package org.mbari.vcr4j.vlcj;
+
+import org.mbari.vcr4j.VideoState;
+
+/**
+ * @author Brian Schlining
+ * @since 2016-04-18T15:44:00
+ */
+public class MediaPlayerState implements VideoState {
+
+
+}
---
+++ vcr4j-vlcj/src/main/java/org/mbari/vcr4j/vlcj/MediaPlayerVideoIO.java
@@ -0,0 +1,8 @@
+package org.mbari.vcr4j.vlcj;
+
+/**
+ * @author Brian Schlining
+ * @since 2016-04-18T15:42:00
+ */
+public class MediaPlayerVideoIO {
+}
