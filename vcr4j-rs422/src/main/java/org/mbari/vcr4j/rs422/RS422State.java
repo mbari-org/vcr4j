ceb98fa95f93167c7239b5598ae761693a14c886
Brian Schlining <bschlining@gmail.com>
2019-07-25 15:06:13 -0700

    check point

--- vcr4j-rs422/src/main/java/org/mbari/vcr4j/rs422/RS422State.java
+++ vcr4j-rs422/src/main/java/org/mbari/vcr4j/rs422/RS422State.java
@@ -8,16 +8,14 @@ import java.util.Arrays;
 
 public class RS422State implements VideoState {
 
-
     /** Corrected LTC time data */
     public static final byte[] OTHER_STATUS_REPLY = { 0x71, 0x20 };
 
     public static final byte[] STATUS_REPLY = { 0x73, 0x20 };
-    
+
     /**
      * bit representing bad communications. 1 = bad comuunications, 0 =
-     * communications are OK. IMplementations should not depend on this bit but should watch for BadPortExceptions instead.
-     *  @see org.mbari.comm.BadPortException
+     * communications are OK.
      */
     public final static long STS_BAD_COMM = 0x0000080L;
 
@@ -25,24 +23,27 @@ public class RS422State implements VideoState {
     public final static long STS_CUE_UP = 0x0010000L;
 
     /**
-     * Bit representing direction, 1 = reverse, 0= forward Sony documentation states STS_DIRECTION    = 0x0040000L.
-     * However we found it is STS_DIRECTION    = 0x0000004L
+     * Bit representing direction, 1 = reverse, 0= forward Sony documentation states
+     * STS_DIRECTION = 0x0040000L. However we found it is STS_DIRECTION = 0x0000004L
      */
     public final static long STS_DIRECTION = 0x0000004L;
 
-    // public static final long STS_DIRECTION    = 0x0040000L;
+    // public static final long STS_DIRECTION = 0x0040000L;
 
-    /** Bit representing fast-forward mode. 1 if the VCR is fast-forwarding, 0 otherwise */
+    /**
+     * Bit representing fast-forward mode. 1 if the VCR is fast-forwarding, 0
+     * otherwise
+     */
     public final static long STS_FAST_FWD = 0x0000400L;
 
     /**
-     * bit representing hardware error, 1 = error, 0 = no error. A typical error
-     * is due to overheating. NOTE: Documentation states: STS_HW_ERROR     = 0x0000004L;
+     * bit representing hardware error, 1 = error, 0 = no error. A typical error is
+     * due to overheating. NOTE: Documentation states: STS_HW_ERROR = 0x0000004L;
      * However we found that is used by the VCR to indicate direction
      */
     public final static long STS_HW_ERROR = 0x0000000L;
 
-    // public static final long STS_HW_ERROR     = 0x0000004L;
+    // public static final long STS_HW_ERROR = 0x0000004L;
 
     /** bit representing jog mode, 1 = jog mode, 0 = not jog mode. */
     public final static long STS_JOG = 0x0100000L;
@@ -63,71 +64,94 @@ public class RS422State implements VideoState {
     public final static long STS_REWIND = 0x0000800L;
 
     /**
-     * Bit representing servo lock. 1 when the VCR is in the following conditions:<br> <ol>
+     * Bit representing servo lock. 1 when the VCR is in the following
+     * conditions:<br>
+     * <ol>
      * <li>The drum and capstan servos are locked in play mode</li>
-     * <li>The drum servo is locked in a mode other than play mode</li> <li>During STANDBY OFF</li> </ol>
+     * <li>The drum servo is locked in a mode other than play mode</li>
+     * <li>During STANDBY OFF</li>
+     * </ol>
      */
     public final static long STS_SERVO_LOCK = 0x0800000L;
 
     /**
-     * 1 when the signal selected by the SERVO REF selector on the
-     * front panel is not inputted. (I don't know what this means either, thats a direct quote from the documentation.)
+     * 1 when the signal selected by the SERVO REF selector on the front panel is
+     * not inputted. (I don't know what this means either, thats a direct quote from
+     * the documentation.)
      */
     public final static long STS_SERVO_REF = 0x0000010L;
 
     /**
      * Bit representing shuttle mode, 1 = vcr is shuttiling, 0= is not shuttiling.
-     * Use in combintation with STS_DIRECTION to determine if the VCR is shuttiling forward or in reverse<br><br>
-     * <b>Note: Sony documentation says: STS_SHUTTLE      = 0x0200000L. However we found it to be STS_SHUTTLE      = 0x0000020</b>
+     * Use in combintation with STS_DIRECTION to determine if the VCR is shuttiling
+     * forward or in reverse<br>
+     * <br>
+     * <b>Note: Sony documentation says: STS_SHUTTLE = 0x0200000L. However we found
+     * it to be STS_SHUTTLE = 0x0000020</b>
      */
     public final static long STS_SHUTTLE = 0x0000020L;
 
-    // public static final long STS_SHUTTLE      = 0x0200000L;
+    // public static final long STS_SHUTTLE = 0x0200000L;
 
     /**
      * Bit representing stand-by mode. 1 if the VCR is standing-by, 0 otherwise.
-     * Note: The VCR always seems to be standing-by. I'm not sure what would cause the VCR to NOT stand-by.
+     * Note: The VCR always seems to be standing-by. I'm not sure what would cause
+     * the VCR to NOT stand-by.
      */
     public final static long STS_STANDBY = 0x0008000L;
 
     /**
-     * bit reprepresenting still status, 1 when the tape stops during stop or search mode and the
-     * CONTROLLED DEVICE goes into the still mode. (Again, not sure exactly, when this occurs. Use isStopped() instead.
+     * bit reprepresenting still status, 1 when the tape stops during stop or search
+     * mode and the CONTROLLED DEVICE goes into the still mode. (Again, not sure
+     * exactly, when this occurs. Use isStopped() instead.
      */
     public final static long STS_STILL = 0x0020000L;
 
     /** Bit representing stop mode. 1 if the VCR is stopped, 0 otherwise */
     public final static long STS_STOP = 0x0002000L;
 
-    /** Bit indicating if the end of the tape has been reached. 1 if it's the end of the tape, 0 otherwise */
+    /**
+     * Bit indicating if the end of the tape has been reached. 1 if it's the end of
+     * the tape, 0 otherwise
+     */
     public final static long STS_TAPE_END = 0x4000000L;
 
     /** bit representing tape trouble, 1 = trouble, 0 = no trouble. */
     public final static long STS_TAPE_TROUBLE = 0x0000008L;
 
-    /** Bit representing auto edit mode, 1 when the VCR is in auto edit mode, 0 otherwise. */
+    /**
+     * Bit representing auto edit mode, 1 when the VCR is in auto edit mode, 0
+     * otherwise.
+     */
     public final static long STS_TSO = 0x0400000L;
 
-    /** WARNING: This bit maks is incorrectly defined in the sony documentation. It is currently no implemented */
+    /**
+     * WARNING: This bit maks is incorrectly defined in the sony documentation. It
+     * is currently no implemented
+     */
     public final static long STS_UNTHREAD = 0x0000000L;
 
-    // public static final long STS_UNTHREAD     = 0x0000020L;
+    // public static final long STS_UNTHREAD = 0x0000020L;
 
     /**
-     * bit representing var mode. 1 when the VCR is in the following modes:<br> <ol> <li>VAR SLOW mode</li>
-     * <li>VAR MEMORY PLAY mode</li> <li>CAPSTAN OVERRIDE mode</li> </ol>
+     * bit representing var mode. 1 when the VCR is in the following modes:<br>
+     * <ol>
+     * <li>VAR SLOW mode</li>
+     * <li>VAR MEMORY PLAY mode</li>
+     * <li>CAPSTAN OVERRIDE mode</li>
+     * </ol>
      */
     public final static long STS_VARSPEED = 0x0080000L;
     private static final Logger log = LoggerFactory.getLogger(RS422State.class);
     public static final RS422State STOPPED = new RS422State(STS_STOP);
-    
+
     private final long status;
-    
+
     public RS422State(long status) {
         this.status = status;
         logStatus();
     }
-    
+
     /**
      * TODO 20031001 brian: What does this mean? Not defined in the 9-pin protocol
      *
@@ -142,7 +166,10 @@ public class RS422State implements VideoState {
         return status > 0;
     }
 
-    /** @return True when a cue-up operation is completed. Reruns false as soon as the tape starts running. */
+    /**
+     * @return True when a cue-up operation is completed. Reruns false as soon as
+     *         the tape starts running.
+     */
     @Override
     public boolean isCueingUp() {
         return ((status & STS_CUE_UP) > 0);
@@ -155,9 +182,10 @@ public class RS422State implements VideoState {
     }
 
     /**
-     * @return True if a hardware error occurs in the Sony VCR. Errors may be due
-     * to power supply overheating, motor drive circuitry overheating, tape path
-     * system error, or communication error between servo and system control.
+     * @return True if a hardware error occurs in the Sony VCR. Errors may be due to
+     *         power supply overheating, motor drive circuitry overheating, tape
+     *         path system error, or communication error between servo and system
+     *         control.
      */
     public boolean isHardwareError() {
         return ((status & STS_HW_ERROR) > 0);
@@ -170,8 +198,8 @@ public class RS422State implements VideoState {
 
     /**
      * @return True when the REMOTE/LOCAL switch on the front panel is set to
-     * <b>LOCAL</B> or when the REMOTE/LOCAL switch is set to <B>REMOTE</B> and
-     * the REMOTE switch S6 on the SY-70 board is set to REMOTE-3
+     *         <b>LOCAL</B> or when the REMOTE/LOCAL switch is set to <B>REMOTE</B>
+     *         and the REMOTE switch S6 on the SY-70 board is set to REMOTE-3
      */
     public boolean isLocal() {
         return ((status & STS_LOCAL) > 0);
@@ -190,7 +218,8 @@ public class RS422State implements VideoState {
 
     /**
      * @return True if the tape in the vcr is moving backwards, i.e. during rewinds
-     * shuttiling backwards, or reverse playing. False if the VCR is playing, fast-forwarding, or shuttiling forwards
+     *         shuttiling backwards, or reverse playing. False if the VCR is
+     *         playing, fast-forwarding, or shuttiling forwards
      */
     @Override
     public boolean isReverseDirection() {
@@ -204,17 +233,21 @@ public class RS422State implements VideoState {
     }
 
     /**
-     * @return True when the VCR is in the following conditions:<br> <ol>
-     * <li>The drum and capstan servos are locked in play mode</li>
-     * <li>The drum servo is locked in a mode other than play mode</li> <li>During STANDBY OFF</li> </ol>
+     * @return True when the VCR is in the following conditions:<br>
+     *         <ol>
+     *         <li>The drum and capstan servos are locked in play mode</li>
+     *         <li>The drum servo is locked in a mode other than play mode</li>
+     *         <li>During STANDBY OFF</li>
+     *         </ol>
      */
     public boolean isServoLock() {
         return ((status & STS_SERVO_LOCK) > 0);
     }
 
     /**
-     * @return True when the signal selected by the SERVO REF selector on the
-     * front panel is not inputted. (I don't know what this means either, thats a direct quote from the documentation.)
+     * @return True when the signal selected by the SERVO REF selector on the front
+     *         panel is not inputted. (I don't know what this means either, thats a
+     *         direct quote from the documentation.)
      */
     public boolean isServoRef() {
         return ((status & STS_SERVO_REF) > 0);
@@ -222,8 +255,9 @@ public class RS422State implements VideoState {
 
     /**
      * @return True if the VCR is in shuttiling mode (either forward or reverse).
-     * Use the <i>isReverseDirection()</i> method in conjuncton with this
-     * <i>isShuttling</i> to determine if the VCR is shuttiling forward or backwards.
+     *         Use the <i>isReverseDirection()</i> method in conjuncton with this
+     *         <i>isShuttling</i> to determine if the VCR is shuttiling forward or
+     *         backwards.
      */
     @Override
     public boolean isShuttling() {
@@ -232,9 +266,9 @@ public class RS422State implements VideoState {
 
     /**
      * @return True if the VCR is Standying by. (i.e When the CONTROLLED DEVICE
-     * recieves the "20.05: standby on" command and goes into stand-by on mode.
-     * Note: I've never seen the VCR return anything but true for this status
-     * bit. I'm not sure what would cause the VCR to NOT standby
+     *         recieves the "20.05: standby on" command and goes into stand-by on
+     *         mode. Note: I've never seen the VCR return anything but true for this
+     *         status bit. I'm not sure what would cause the VCR to NOT standby
      */
     public boolean isStandingBy() {
         return ((status & STS_STANDBY) > 0);
@@ -242,7 +276,8 @@ public class RS422State implements VideoState {
 
     /**
      * @return True when the tape stops during stop or search mode and the
-     * CONTROLLED DEVICE goes into the still mode. (Again, not sure exactly, when this occurs. Use isStopped() instead.
+     *         CONTROLLED DEVICE goes into the still mode. (Again, not sure exactly,
+     *         when this occurs. Use isStopped() instead.
      */
     public boolean isStill() {
         return ((status & STS_STILL) > 0);
@@ -275,13 +310,17 @@ public class RS422State implements VideoState {
     }
 
     /**
-     * @return True when the VCR is in the following modes:<br> <ol> <li>VAR SLOW mode</li> <li>VAR MEMORY PLAY mode</li>
-     * <li>CAPSTAN OVERRIDE mode</li> </ol>
+     * @return True when the VCR is in the following modes:<br>
+     *         <ol>
+     *         <li>VAR SLOW mode</li>
+     *         <li>VAR MEMORY PLAY mode</li>
+     *         <li>CAPSTAN OVERRIDE mode</li>
+     *         </ol>
      */
     public boolean isVarSpeed() {
         return ((status & STS_VARSPEED) > 0);
     }
-    
+
     private void logStatus() {
 
         if (log.isDebugEnabled()) {
@@ -374,9 +413,7 @@ public class RS422State implements VideoState {
     }
 
     public static boolean isStatusReply(byte[] cmd) {
-        return (Arrays.equals(cmd, STATUS_REPLY)
-                || Arrays.equals(cmd, OTHER_STATUS_REPLY));
+        return (Arrays.equals(cmd, STATUS_REPLY) || Arrays.equals(cmd, OTHER_STATUS_REPLY));
     }
-    
-    
+
 }
