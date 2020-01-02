50e37530f6367330c18be8416392a9a4110459ca
Brian Schlining <bschlining@gmail.com>
2020-01-02 15:27:22 -0800

    check point

--- vcr4j-examples/src/main/java/org/mbari/vcr4j/examples/jssc/VARSDemo01.java
+++ vcr4j-examples/src/main/java/org/mbari/vcr4j/examples/jssc/VARSDemo01.java
@@ -32,60 +32,60 @@ public class VARSDemo01 {
 
         Logger log = LoggerFactory.getLogger(VARSDemo01.class);
 
-        JSSCVideoIO io = JSSCVideoIO.open(portName);
-
-        // --- Decorate VideoIO like we would in VARS
-        // VARS uses this decorator to keep the UI in sync with the VCR state and timecode
-        // it uses a timer to regularly schedule requests.
-        Decorator syncDecorator = new VCRSyncDecorator<>(io);
-
-        // Some commands should do a status request immediately to update the UI state.
-        // This decorator listens for those commands follows them up with a status update
-        Decorator statusDecorator = new RS422StatusDecorator(io);
-
-        // We write time to the userbits. This decorator adds userbits decoding and support
-        // for Timestamp commands. Listen to it's observable for fancy indices
-        UserbitsAsTimeDecorator timeDecorator = new UserbitsAsTimeDecorator(io);
-
-        // Log updates to our fancy time index
-        timeDecorator.getIndexObservable()
-                .subscribe(vi -> log.debug("USERBITS: " + new VideoIndexAsString(vi).toString()));
-
-        // --- Execute Play and just see what the traffic does.
-        io.send(VideoCommands.PLAY);
-
-        // Wait until video starts playing. Sometimes the VCR take a few seconds to get rolling
-        AtomicBoolean wait = new AtomicBoolean(true);
-        io.getStateObservable()
-                .filter(RS422State::isPlaying)
-                .take(1)
-                .forEach(state -> {
-                    log.info("THE DEVICE IS PLAYING");
-                    wait.set(false);
-                });
-
-        while (wait.get()) {
-            Thread.sleep(200);
-        }
-
-        // When we grab annotations in VARS we need to get the video index. Requesting a timestamp when
-        // The UserbitsAsTimeDecorator is attached will request both a timecode and userbits.
-        log.info("DEVICE IS IN PLAY MODE. REQUESTING TIME AT 1 SEC INTERVALS");
-        for (int i = 0; i < 5; i++) {
-            io.send(VideoCommands.REQUEST_TIMESTAMP);
-            Thread.sleep(1000);
-        }
-
-        // --- Shut down
-        // Close everything when the VCR stops
-        io.getStateObservable()
-                .filter(RS422State::isStopped)
-                .take(1)
-                .forEach(state -> io.close());
-
-        // e.g. Fewer interrupted exceptions form the VCRSyncDecorator
-        log.info("STOPPING DEVICE");
-        io.send(VideoCommands.STOP);
+//        JSSCVideoIO io = JSSCVideoIO.open(portName);
+//
+//        // --- Decorate VideoIO like we would in VARS
+//        // VARS uses this decorator to keep the UI in sync with the VCR state and timecode
+//        // it uses a timer to regularly schedule requests.
+//        Decorator syncDecorator = new VCRSyncDecorator<>(io);
+//
+//        // Some commands should do a status request immediately to update the UI state.
+//        // This decorator listens for those commands follows them up with a status update
+//        Decorator statusDecorator = new RS422StatusDecorator(io);
+//
+//        // We write time to the userbits. This decorator adds userbits decoding and support
+//        // for Timestamp commands. Listen to it's observable for fancy indices
+//        UserbitsAsTimeDecorator timeDecorator = new UserbitsAsTimeDecorator(io);
+//
+//        // Log updates to our fancy time index
+//        timeDecorator.getIndexObservable()
+//                .subscribe(vi -> log.debug("USERBITS: " + new VideoIndexAsString(vi).toString()));
+//
+//        // --- Execute Play and just see what the traffic does.
+//        io.send(VideoCommands.PLAY);
+//
+//        // Wait until video starts playing. Sometimes the VCR take a few seconds to get rolling
+//        AtomicBoolean wait = new AtomicBoolean(true);
+//        io.getStateObservable()
+//                .filter(RS422State::isPlaying)
+//                .take(1)
+//                .forEach(state -> {
+//                    log.info("THE DEVICE IS PLAYING");
+//                    wait.set(false);
+//                });
+//
+//        while (wait.get()) {
+//            Thread.sleep(200);
+//        }
+//
+//        // When we grab annotations in VARS we need to get the video index. Requesting a timestamp when
+//        // The UserbitsAsTimeDecorator is attached will request both a timecode and userbits.
+//        log.info("DEVICE IS IN PLAY MODE. REQUESTING TIME AT 1 SEC INTERVALS");
+//        for (int i = 0; i < 5; i++) {
+//            io.send(VideoCommands.REQUEST_TIMESTAMP);
+//            Thread.sleep(1000);
+//        }
+//
+//        // --- Shut down
+//        // Close everything when the VCR stops
+//        io.getStateObservable()
+//                .filter(RS422State::isStopped)
+//                .take(1)
+//                .forEach(state -> io.close());
+//
+//        // e.g. Fewer interrupted exceptions form the VCRSyncDecorator
+//        log.info("STOPPING DEVICE");
+//        io.send(VideoCommands.STOP);
 
     }
 }
