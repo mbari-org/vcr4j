b353a3d3a052e876f4aab0f43d14401322726191
Brian Schlining <bschlining@gmail.com>
2023-01-24 14:35:01 -0800

    mbari-org/vars-feedback#39 - Sanity test added to vcr4j

---
+++ vcr4j-examples/src/main/java/org/mbari/vcr4j/examples/remote/Issue39.java
@@ -0,0 +1,24 @@
+package org.mbari.vcr4j.examples.remote;
+
+import org.mbari.vcr4j.remote.control.commands.OpenCmd;
+import org.mbari.vcr4j.remote.control.commands.RemoteCommands;
+
+public class Issue39 {
+
+    public static void main(String[] args) throws Exception {
+        var appArgs = AppArgs.parse(args, Issue39.class.getName());
+        var rc = appArgs.remoteControl();
+        var uuid = appArgs.getVideoUuid();
+        var url = appArgs.url();
+        var io = rc.getVideoIO();
+
+        io.send(new OpenCmd(uuid, url));
+
+        Thread.sleep(2000);
+
+        for (int i = 0; i < 1000; i++) {
+            io.send(RemoteCommands.FRAMEADVANCE);
+            Thread.sleep(60);
+        }
+    }
+}
