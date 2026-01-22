1c38ecd5833c0064be91e299b2bda2dfbb1050b2
Brian Schlining <bschlining@gmail.com>
2026-01-21 16:31:05 -0800

    Added Apache 2 LICENSE

--- vcr4j-jserialcomm/src/main/java/org/mbari/vcr4j/jserialcomm/SerialCommVideoIO.java
+++ vcr4j-jserialcomm/src/main/java/org/mbari/vcr4j/jserialcomm/SerialCommVideoIO.java
@@ -1,5 +1,25 @@
 package org.mbari.vcr4j.jserialcomm;
 
+/*-
+ * #%L
+ * vcr4j-jserialcomm
+ * %%
+ * Copyright (C) 2008 - 2026 Monterey Bay Aquarium Research Institute
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.fazecast.jSerialComm.SerialPort;
 import org.mbari.vcr4j.rs422.RS422Exception;
 import org.mbari.vcr4j.rs422.RS422ResponseParser;
